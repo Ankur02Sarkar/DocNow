caee6b61b101c9d18c71696c439560e30c203129
curateit-dev <save@curateit.com>
2023-09-25 15:15:10 +0530

    update

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
 
 ////////////////////// Comment out for Development ///////////////////////////////////////////////
 // files for production
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
