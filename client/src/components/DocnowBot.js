48d0714433d360be0bcd06117fb4d9551e9c9b81
curateit-dev <save@curateit.com>
2023-09-25 20:08:22 +0530

    update

--- client/src/components/DocnowBot.js
+++ client/src/components/DocnowBot.js
@@ -1,16 +1,14 @@
 import React from "react";
-
+import Layout from "./Layout"
 const DocnowBot = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-200">
-      <div className="border rounded-lg overflow-hidden shadow-lg w-full md:w-3/4 lg:w-1/2">
-        <iframe
-          src="https://mediafiles.botpress.cloud/bc81f148-1c4b-4648-ad5a-88410ac7dc7a/webchat/bot.html"
-          title="Bot Chat"
-          className="w-full h-[500px] border-none"
-        ></iframe>
-      </div>
-    </div>
+    <Layout>
+      <iframe
+        src="https://docnow-bot.vercel.app/"
+        title="Bot Chat"
+        className="w-full h-[500px] border-none"
+      ></iframe>
+    </Layout>
   );
 };
 
